0e3bb727a71f6a23fc16fb019141076efb6dced1
Kpallavi26 <kpallavi6307@gmail.com>
2026-02-04 10:21:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.small"]
       desired_size   = 2
       min_size       = 1
       max_size       = 3
